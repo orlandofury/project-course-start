4dbc63ea57e685e54fb7ce06d6c3cb53d21f3199
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-15 02:30:13 -0600

    Section 10: clean up binding calls with RecipeService

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe','This is simple a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
         new Recipe('A Test Recipe 2','This is simple a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -8,11 +8,18 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipes.component.css'],
   providers:[RecipeService]
 })
-export class RecipesComponent  {
+export class RecipesComponent implements OnInit  {
   selectedRecipe: Recipe;
 
-  constructor() {
+  constructor(private recipeService:RecipeService) {
     
+  }
+  ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe(
+      ( recipe:Recipe ) => {
+        this.selectedRecipe = recipe;
+      }
+    )
   }
 
 
