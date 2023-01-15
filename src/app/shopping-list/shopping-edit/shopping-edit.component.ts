85fe357c81c272e7c825134bb1ba0ffc4ec56f34
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-15 02:52:18 -0600

    Section 10: Adding shopping list service and using the emitter in the service to pass data

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 
 @Component({
@@ -10,12 +11,14 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent {
   @ViewChild('nameInput', {static:true}) nameInputRef:ElementRef
   @ViewChild('amountInput', {static:true}) amountInputRef:ElementRef
-  @Output() ingredientAdded = new EventEmitter<Ingredient>()  
   
+  constructor(private shoppingListService: ShoppingListService) {
+    
+  }
   onAddItem(){
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName,ingAmount)
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
