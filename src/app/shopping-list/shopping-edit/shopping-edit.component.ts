84fac72990c48c9f204ab3034f9580ee8957b368
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-16 02:20:01 -0600

    Section 16 template driven shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -36,9 +36,24 @@ export class ShoppingEditComponent implements OnInit,OnDestroy {
       }
     );
   }
-  onAddItem(form:NgForm){
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+  onDelete(){
+    this.shoppingListService.deleteIngredients(this.editedItemIndex);
+    this.onClear();
+  }
+  onSubmit(form:NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name,value.amount)
-    this.shoppingListService.addIngredient(newIngredient);
+    if(this.editMode){
+      this.shoppingListService.updateIngredients(this.editedItemIndex,newIngredient);
+    }
+    else{
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.editMode=false;
+    form.reset();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -24,4 +24,12 @@ export class ShoppingListService{
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+    updateIngredients(index: number, newIngredient:Ingredient){
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+    deleteIngredients(index:number){
+        this.ingredients.splice(index,1);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
