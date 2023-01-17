b7ac74d8ef5a7f621848742b8514e56e4ae61527
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-16 22:21:23 -0600

    Adding Firestore realtime database, setting and fetching the database using httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -34,6 +35,7 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService,RecipeService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -27,7 +27,10 @@ export class RecipeService{
       ];
     
     constructor(private slService: ShoppingListService){}
-    
+    setRecipes(recipes: Recipe []){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
     getRecipe(index: number){
         return this.recipes[index];
     }
